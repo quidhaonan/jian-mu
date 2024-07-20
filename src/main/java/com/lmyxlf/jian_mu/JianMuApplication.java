62c09e478ae1d7065339dc0aeb21dbe9b36f6b6c
lmy <2804189910@qq.com>
2024-07-21 03:35:14 +0800

    fix:开启任务调度

--- src/main/java/com/lmyxlf/jian_mu/JianMuApplication.java
+++ src/main/java/com/lmyxlf/jian_mu/JianMuApplication.java
@@ -3,7 +3,9 @@ package com.lmyxlf.jian_mu;
 import com.lmyxlf.jian_mu.global.config.annotation.EnableCommonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableCommonConfig
 @SpringBootApplication
 public class JianMuApplication {
